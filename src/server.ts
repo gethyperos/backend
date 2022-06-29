ffe2e432592c487b566d2ed8329b24f4c46aa71d
nodgear <matheus.silva3@ftc.edu.br>
2022-06-29 20:57:08 -0300

    feat: prevent server from starting without secret

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import { internalIpV4 } from 'internal-ip'
 import logger from 'signale'
+import dotenv from 'dotenv'
 import app from './app'
 
+dotenv.config()
+
 const port = process.env.PORT || 5001
+const token = process.env.JWT_SECRET
+
+if (!token) {
+  logger.error('JWT_SECRET is not defined')
+  process.exit(1)
+}
 
 app.listen(port, () => {
   internalIpV4().then((internalIP) => {
