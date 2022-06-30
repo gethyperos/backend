c401f2d13aeaffcf3713a6874287c39262bdc162
nodgear <matheus.silva3@ftc.edu.br>
2022-06-30 02:06:57 -0300

    feat: generate JWT_TOKEN on first run

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,25 @@
 import { internalIpV4 } from 'internal-ip'
-import logger from 'signale'
+import signale from 'signale'
 import dotenv from 'dotenv'
+import { v4 as uuidv4 } from 'uuid'
+import { appendFileSync } from 'fs'
 import app from './app'
 
 dotenv.config()
 
 const port = process.env.PORT || 5001
 const token = process.env.JWT_SECRET
+const interactiveLog = new signale.Signale({ interactive: true })
+const logger = new signale.Signale()
 
 if (!token) {
-  logger.error('JWT_SECRET is not defined')
-  process.exit(1)
+  interactiveLog.error('[%d/3] JWT_SECRET is not defined', 1)
+  interactiveLog.start('[%d/3] Generating token...', 2)
+  const generatedToken = uuidv4()
+  interactiveLog.success(`[%d/3] Token generated: ${generatedToken}`, 3)
+
+  appendFileSync('.env', `\nJWT_SECRET="${generatedToken}"`)
+  process.env.JWT_SECRET = generatedToken
 }
 
 app.listen(port, () => {
