b982597e9a00faa9f391e4a4d43ef2c751f99f15
nodgear <matheus.silva3@ftc.edu.br>
2022-07-05 04:03:05 -0300

    🔥 REMOVE (SYSTEM): unused getConfigByKey method

--- src/controllers/config.controllers.ts
+++ src/controllers/config.controllers.ts
@@ -5,47 +5,24 @@ import { searchWithParameters } from '@util/filtering'
 import { ensureCache } from '@root/cache/apiCache'
 
 export async function getConfigs(req: Request, res: Response, next: NextFunction) {
+  const filter = req.query
+  const { configKey } = req.params
+
   try {
     const configs = await ensureCache('systemConfigs', async () => {
       const configsDB = getConfigsDB()
       return configsDB
     })
 
+    const filteredConfigs = searchWithParameters(configKey ? { key: configKey } : filter, configs)
     res.status(200).json({
-      configs,
+      configs: filteredConfigs,
     })
   } catch (e) {
     next({
       statusCode: 500,
       error: `${e}`,
       message: 'Failed to retrieve configs.',
-    })
-  }
-}
-
-export async function getConfigByKey(req: Request, res: Response, next: NextFunction) {
-  const { key } = req.params
-
-  try {
-    const configs = await ensureCache('systemConfigs', async () => {
-      const configsDB = getConfigsDB()
-      return configsDB
-    })
-
-    const config = searchWithParameters(configs, { key })
-
-    if (!config) {
-      throw new Error('Config not found')
-    }
-
-    res.status(200).json({
-      config,
-    })
-  } catch (e) {
-    next({
-      statusCode: 404,
-      error: `${e}`,
-      message: 'Unable to retrieve config.',
     })
   }
 }
--- src/routes/system.routes.ts
+++ src/routes/system.routes.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 
-import { getConfigs, getConfigByKey, updateConfig } from '@controller/config.controllers'
+import { getConfigs, updateConfig } from '@controller/config.controllers'
 import { listRunningContainers, listDownloadedImages } from '@controller/docker.controllers'
 import authMiddleware from '@middleware/auth.middleware'
 
 const router = express.Router()
 
 router.get('/configs', authMiddleware, getConfigs)
-router.get('/configs/:configKey', authMiddleware, getConfigByKey)
+router.get('/configs/:configKey', authMiddleware, getConfigs)
 router.put('/configs', authMiddleware, updateConfig)
 router.get('/docker/containers', authMiddleware, listRunningContainers)
 router.get('/docker/images', authMiddleware, listDownloadedImages)
