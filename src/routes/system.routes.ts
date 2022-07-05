2864a604cef157c45841509194db9ea18d811701
nodgear <matheus.silva3@ftc.edu.br>
2022-07-05 00:50:47 -0300

    fix: missing endpoints on system routes

--- src/routes/system.routes.ts
+++ src/routes/system.routes.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 
+import { listConfigs } from '@controller/config.controllers'
+import { listRunningContainers, listDownloadedImages } from '@controller/docker.controllers'
+import authMiddleware from '@middleware/auth.middleware'
+
 const router = express.Router()
 
-router.post('')
-router.get('')
-router.patch('')
+router.get('/docker/containers', authMiddleware, listRunningContainers)
+router.get('/docker/images', authMiddleware, listDownloadedImages)
+router.get('/configs', authMiddleware, listConfigs)
 
 export default router
