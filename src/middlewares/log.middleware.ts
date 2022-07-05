ef97424cfe8ade1543e02c12c6ed51334ee89fc8
nodgear <matheus.silva3@ftc.edu.br>
2022-07-05 04:14:16 -0300

    🎨 IMPROVE: log level debug log middleware)

--- src/middlewares/log.middleware.ts
+++ src/middlewares/log.middleware.ts
@@ -3,6 +3,6 @@ import signale from 'signale'
 
 export default function logMiddleware(req: Request, res: Response, next: NextFunction) {
   const logger = signale.scope(req.method)
-  logger.log(`${req.path}\n└─ Params ${JSON.stringify(req.params)}`)
+  logger.debug(`${req.path}`)
   next()
 }
