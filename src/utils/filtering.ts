b1366bceaa6878c29adc340e4007ae8056a445fb
nodgear <matheus.silva3@ftc.edu.br>
2022-07-05 00:45:40 -0300

    feat: filter & parsing utility functions

--- src/utils/filtering.ts
+++ src/utils/filtering.ts
@@ -8,13 +8,13 @@ export function searchWithParameters(filters: RequestQuery, objects: any) {
     let isValid = true
     // eslint-disable-next-line no-restricted-syntax
     for (const key in filters) {
-      if (filters[key] !== object[key]) {
+      // eslint-disable-next-line eqeqeq
+      if (filters[key] != object[key]) {
         isValid = false
       }
     }
     return isValid
   })
-
   return filteredObjects
 }
 
@@ -26,7 +26,59 @@ export function castValueType(value: any, type: string) {
       return Number(value)
     case 'boolean':
       return Boolean(value)
+    case 'array':
+      return value.split(',')
     default:
-      return value
+      return String(value)
+  }
+}
+
+export function castTypeValue(value: any) {
+  switch (typeof value) {
+    case 'string':
+      return 'string'
+    case 'number':
+      return 'number'
+    case 'boolean':
+      return 'number'
+    default:
+      return Array.isArray(value) ? 'array' : typeof value
+  }
+}
+
+export function castConfigData(key: string, value: number | string | boolean | any[]) {
+  const typeCast = castTypeValue(value)
+  const valueCast = Array.isArray(value) ? value.join(',') : String(value)
+
+  return {
+    key,
+    value: valueCast,
+    type: typeCast,
+  }
+}
+
+export function castDockerConn(
+  str: string
+): { socketPath: string } | { protocol: 'http'; host: string; port: string } {
+  const unixPathRegex =
+    /((\.{2}\/{1})+|((\.{1}\/{1})?)|(\/{1}))(([a-zA-Z0-9]+\/{1})+)([a-zA-Z0-9])+(\.{1}[a-zA-Z0-9]+)?$/
+
+  // Given URI is a unix socket (unix path)
+  if (unixPathRegex.test(str)) {
+    if (!str.endsWith('.sock')) {
+      throw new Error('Invalid path for Socket URI')
+    }
+
+    return {
+      socketPath: str,
+    }
+  }
+  if (!str.startsWith('http://')) {
+    throw new Error('Invalid protocol for Docker URI')
+  }
+  return {
+    protocol: 'http',
+    host: str.replace('http://', '').split(':')[0],
+    port: str.replace('http://', '').split(':')[1],
   }
 }
