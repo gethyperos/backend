8380509627427182f2b3f83ab8f9164a535e9ff9
nodgear <matheus.silva3@ftc.edu.br>
2022-07-05 06:02:56 -0300

    ✨ NEW FEATURE (system): Docker images endpoint

--- src/controllers/docker.controllers.ts
+++ src/controllers/docker.controllers.ts
@@ -1,23 +1,36 @@
 import { NextFunction, Request, Response } from 'express'
 import { listContainers, listImages } from '@service/docker.services'
+import { searchWithParameters } from '@util/filtering'
 
-export async function listRunningContainers(req: Request, res: Response, next: NextFunction) {
+export async function getContainers(req: Request, res: Response, next: NextFunction) {
+  const { containerId } = req.params
+  const filters = req.query
+
   try {
-    const apps = await listContainers()
-    res.json({ apps })
+    const containers = await listContainers()
+    const filteredContainers = searchWithParameters(
+      containerId ? { Id: containerId } : filters,
+      containers
+    )
+
+    res.json({ containers: filteredContainers })
   } catch (e) {
     next({
       statusCode: 500,
       message: `${e}`,
-      error: 'Unable to fetch installed apps',
+      error: 'Unable to retrieve containers',
     })
   }
 }
 
-export async function listDownloadedImages(req: Request, res: Response, next: NextFunction) {
+export async function getImages(req: Request, res: Response, next: NextFunction) {
+  const { imageId } = req.params
+  const filters = req.query
+
   try {
-    const apps = await listImages()
-    res.json({ apps })
+    const images = await listImages()
+    const filteredImages = searchWithParameters(imageId ? { Id: imageId } : filters, images)
+    res.json({ images: filteredImages })
   } catch (e) {
     next({
       statusCode: 500,
--- src/routes/system.routes.ts
+++ src/routes/system.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 
 import { getConfigs, updateConfig } from '@controller/config.controllers'
-import { listRunningContainers, listDownloadedImages } from '@controller/docker.controllers'
+import { getContainers, getImages } from '@controller/docker.controllers'
 import authMiddleware from '@middleware/auth.middleware'
 
 const router = express.Router()
@@ -9,7 +9,10 @@ const router = express.Router()
 router.get('/configs', authMiddleware, getConfigs)
 router.get('/configs/:configKey', authMiddleware, getConfigs)
 router.put('/configs', authMiddleware, updateConfig)
-router.get('/docker/containers', authMiddleware, listRunningContainers)
-router.get('/docker/images', authMiddleware, listDownloadedImages)
+// Do not cache system information routes.
+// Ex: Containers, Images, CPU, RAM, Disk, etc.
+router.get('/containers', authMiddleware, getContainers)
+router.get('/containers/:containerId', authMiddleware, getContainers)
+router.get('/images/:image', authMiddleware, getImages)
 
 export default router
