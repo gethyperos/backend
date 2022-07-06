bd9dd45c430d6e64346b29555d4795a5fe7a2759
nodgear <matheus.silva3@ftc.edu.br>
2022-07-06 13:51:13 -0300

    ✨ NEW FEATURE: swagger docs in '/ '

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import { internalIpV4 } from 'internal-ip'
 import signale from 'signale'
 import dotenv from 'dotenv'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
 import app from './app'
 
 dotenv.config()
 
 const port = process.env.PORT || 5001
 const logger = new signale.Signale()
+const apiSpecs = YAML.load('api-spec.yaml')
 
+app.use('/', swaggerUi.serve, swaggerUi.setup(apiSpecs))
 app.listen(port, () => {
   internalIpV4().then((internalIP) => {
     const scope = logger.scope('⚡ HyperOS')
