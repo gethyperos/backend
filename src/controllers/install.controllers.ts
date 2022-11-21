796e174c42bd5a004cc9e0de4276b96ce113fd32
nodgear <matheus.silva3@ftc.edu.br>
2022-11-21 08:37:39 -0300

    feat: import containers on install

--- src/controllers/install.controllers.ts
+++ src/controllers/install.controllers.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, Response } from 'express'
-import { checkInstallStatus, startInitialSetup } from '@service/install.services'
+import { checkInstallStatus, startInitialSetup, importContainers } from '@service/install.services'
 import { ensureCache } from '@root/cache/apiCache'
 
 export default async function install(req: Request, res: Response, next: NextFunction) {
-  const { user, hostname } = req.body
+  const { user, hostname, dockerURI } = req.body
 
   if (!user) {
     next({ statusCode: 400, message: 'username is required', error: 'Missing field' })
@@ -11,10 +11,16 @@ export default async function install(req: Request, res: Response, next: NextFun
   if (!hostname) {
     next({ statusCode: 400, message: 'hostname is required', error: 'Missing field' })
   }
+  if (!dockerURI) {
+    next({ statusCode: 400, message: 'dockerURI is required', error: 'Missing field' })
+  }
 
   try {
-    await startInitialSetup(user, hostname)
-    res.status(200).json({ message: 'HyperOS is installed' })
+    await startInitialSetup(user, hostname, dockerURI)
+    const importedContainers = await importContainers()
+    res
+      .status(200)
+      .json({ message: `HyperOS is installed - ${importedContainers} containers imported` })
   } catch (e) {
     next({
       statusCode: 500,
